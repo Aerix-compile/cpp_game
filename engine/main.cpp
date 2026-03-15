f977a2ce979ced17f2789be6e6a5aeb9f68a7509
Aerix-compile <cppgame2026@gmail.com>
2026-03-15 09:24:50 -0400

    Add boot test logic and ignore executable and build files

--- engine/main.cpp
+++ engine/main.cpp
@@ -2,7 +2,19 @@
 
 int main()
 {
-    std::cout << "engine boot ok\n";
+    int bootTestNumber;
 
+    std::cout << "Enter the boot test number: ";
+    std::cin >> bootTestNumber;
+
+    if(bootTestNumber == 1){
+        std::cout << "Boot test succeeded!" << "\n";
+    }
+    else if(bootTestNumber > 1 || bootTestNumber < 0){
+        std::cout << "Boot number entered is invalid." << "\n";
+    }
+    else{
+        std::cout << "Boot test failed." << "\n";
+    }
     return 0;
 }
